88d41ecddbcb33f5b5e0fa86fc020b4aa65dcc74
Luis Herdeth <73970555+luisherdeth2020@users.noreply.github.com>
2023-02-21 13:07:47 +0100

    add allow function api

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 // middleware (antepone el prefijo /api)
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use('/api', todolistRoutes);
